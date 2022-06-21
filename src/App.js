ac0c2abf833c0f1c723dceb9591737557bcf50d2
Tien <ldtien.1993@gmail.com>
2022-06-21 23:58:18 +0700

    add carbrand route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CarBrands from "./pages/CarBrands/CarBrands";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/brand" component={CarBrands} />
       </Switch>
     </BrowserRouter>
   );
